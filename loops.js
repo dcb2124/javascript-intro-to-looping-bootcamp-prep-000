e5062c458a61563398f2603e2c27d9244a1a6e9d
DAVID BILLINGSLEY <dcb2124@gmail.com>
2018-04-01 01:05:24 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,16 @@
+function forLoop(array){
+  
+  for(var i = 1; i < 25; i++){
+    
+    string = `I am ${i} strange loop`
+    
+    if (i == 1){
+      string = string + "."
+    } else {
+      string = string + ".s"
+    }
+    
+    
+  }
+  
+}
